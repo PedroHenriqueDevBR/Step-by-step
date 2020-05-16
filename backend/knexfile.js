f9e3c5b1149785ddbdfd950767f2f05966f897ac
PedroHenriqueDevBR <p.lyma18@gmail.com>
2020-05-15 21:45:45 -0300

    feat: user api done

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,19 +1,10 @@
-// Update with your config settings.
-
 module.exports = {
-
-    client: 'postgresql',
-    connection: {
-      database: 'stepbystep',
-      user:     'postgres',
-      password: 'admin'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-
+  client: 'sqlite3',
+  connection: {
+    filename: './db.sqlite3'
+  },
+  migrations: {
+    directory: './migrations'
+  },
+  useNullAsDefault: true,
 };
--- backend/migrations/20200515110002_create_table_users.js
+++ backend/migrations/20200515110002_create_table_users.js
@@ -2,9 +2,9 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('users', table => {
         table.increments('id').primary()
-        table.string('name').notNUll()
-        table.string('email').notNUll().unique()
-        table.string('password').notNUll()
+        table.string('name').notNull()
+        table.string('email').notNull().unique()
+        table.string('password').notNull()
         table.boolean('admin').notNull().defaultTo(false)
     })
 };
